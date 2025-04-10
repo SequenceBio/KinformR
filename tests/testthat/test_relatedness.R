913981d83786c799da9e1b7c86ac3d32ac985731
CNuge <camnugent@gmail.com>
2025-04-10 15:44:34 -0300

    adding several more unit tests

--- tests/testthat/test_relatedness.R
+++ tests/testthat/test_relatedness.R
@@ -1,12 +1,74 @@
 test_that("Scoring proceeds as expected in all cases", {
 
-  #TODO
-  calc.rv.score
 
-  #TODO
-  score.fam
+  print("test per encoding score calculation for several combos")
+  print("1. simple proband-sib")
+  encoded.dat1 <- list("A_c" = c(0, 1),
+                      "A_i" = c(),
+                      "U_c" = c(),
+                      "U_i" = c())
+
+  expected.score1<- list("score" = 3,
+                         "score.for" = 3,
+                         "score.against" = 0 )
+  score1 <- calc.rv.score(encoded.dat1)
+  expect_equal(expected.score1, score1)
+
+  print("2.  correct A proband and cousin, incorrect unaffected sib")
+  encoded.dat2 <- list("A_c" = c(0, 3),
+                      "A_i" = c(),
+                      "U_c" = c(),
+                      "U_i" = c(1))
+
+  expected.score2<- list("score" = 5,
+                         "score.for" = 9,
+                         "score.against" = 4 )
+  score2 <- calc.rv.score(encoded.dat2)
+  expect_equal(expected.score2, score2)
+
+  print("2. repeat, but only consider affecteds in scoring")
+  score2.no.u <- calc.rv.score(encoded.dat2, unaffected.weight=0)
+  expected.score2.no.u<- list("score" = 9,
+                         "score.for" = 9,
+                         "score.against" = 0 )
+  expect_equal(score2.no.u, expected.score2.no.u)
+
+  print("3.  bad score proband and incorrect unaffected sib")
+
+  encoded.dat3 <- list("A_c" = c(0),
+                      "A_i" = c(),
+                      "U_c" = c(),
+                      "U_i" = c(1))
+
+  expected.score3<- list("score" = -3,
+                         "score.for" = 1,
+                         "score.against" = 3)
+  score3 <- calc.rv.score(encoded.dat3)
+  expect_equal(expected.score3, score3)
+
+  print("3.  complex score, all situations covered in one.")
+
+  encoded.dat4 <- list("A_c" = c(0, 1, 3, 1),
+                       "A_i" = c(3),
+                       "U_c" = c(1, 2),
+                       "U_i" = c(1))
+
+  expected.score4<- list("score" = 7,
+                         "score.for" = 19,
+                         "score.against" = 12 )
+  score1 <- calc.rv.score(encoded.dat4)
+  expect_equal(expected.score4, score4)
+
 
   # TODO
+  #scores
+
+
+
+  #TODO
+  # score.fam
+
+  print("test summing of scores from different families")
   a <- c("score"=10, "score.for"=10, "score.against"=0)
   b <- c("score"=20, "score.for"=20, "score.against"=0)
   c <- c("score"=30, "score.for"=40, "score.against"=10)
