b60a7316e5204bc86e6d4e1dfe4faa8f5297d068
CNuge <camnugent@gmail.com>
2025-05-22 14:26:30 -0300

    docs and testing

--- tests/testthat/test_pedigree_eval.R
+++ tests/testthat/test_pedigree_eval.R
@@ -9,10 +9,16 @@ test_that("Data are read from files correctly", {
   ##################################
   # test 2
   example.pedigree.file2 <-system.file('extdata/example_pedigree_encoding2.tsv', package = 'seqbio.variant.scoring')
-  example.pedigree.file2 <- "inst/extdata/example_pedigree_encoding2.tsv"
+  #example.pedigree.file2 <- "inst/extdata/example_pedigree_encoding2.tsv"
   example.pedigree.df2 <- read.pedigree(example.pedigree.file2)
   penetrance.df2 <- score.pedigree(example.pedigree.df2)
 
+  ##################################
+  # test 3
+  example.pedigree.file3 <-system.file('extdata/example_pedigree_encoding3.tsv', package = 'seqbio.variant.scoring')
+  #example.pedigree.file3 <- "inst/extdata/example_pedigree_encoding3.tsv"
+  example.pedigree.df3 <- read.pedigree(example.pedigree.file3)
+  penetrance.df3 <- score.pedigree(example.pedigree.df3)
 
 
 })
